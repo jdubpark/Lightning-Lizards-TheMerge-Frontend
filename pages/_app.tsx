c8290879bc7f590f77e575ce929f2cb6d62220ff
Jongwon Park <jwparktom@gmail.com>
2022-09-10 15:57:14 -0500

    google tag & favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,7 @@ import {
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
 import { publicProvider } from 'wagmi/providers/public';
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+import Script from "next/script";
 
 const { chains, provider, webSocketProvider } = configureChains(
     [chain.mainnet, chain.goerli, chain.hardhat],
@@ -59,16 +60,29 @@ const lightTheme: Theme = {
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <WagmiConfig client={wagmiClient}>
-            <RainbowKitProvider
-                chains={chains}
-                theme={lightTheme}
-                modalSize="compact"
-                showRecentTransactions
-            >
-                <Component {...pageProps} />
-            </RainbowKitProvider>
-        </WagmiConfig>
+        <>
+            <Script
+                id="google-tag-manager"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-K9JCT38');`
+                }}
+            />
+            <WagmiConfig client={wagmiClient}>
+                <RainbowKitProvider
+                    chains={chains}
+                    theme={lightTheme}
+                    modalSize="compact"
+                    showRecentTransactions
+                >
+                    <Component {...pageProps} />
+                </RainbowKitProvider>
+            </WagmiConfig>
+        </>
     );
 }
 
