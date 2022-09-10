f1f11b0dff49f7a81a485765f76829950dd4fbf5
Jongwon Park <jwparktom@gmail.com>
2022-09-10 16:51:52 -0500

    fvck me I hate prepends

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,3 @@
-export const IS_PRODUCTION = process.env.NODE_ENV === 'production'
+export const IS_PRODUCTION = process.env.NEXT_PUBLIC_NODE_ENV === 'production'
 export const MERGE_CANVAS_CONTRACT_ADDRESS =
-	(IS_PRODUCTION ? process.env.MERGE_CANVAS_CONTRACT_ADDRESS : process.env.MERGE_CANVAS_CONTRACT_ADDRESS_DEV) as string
+	(IS_PRODUCTION ? process.env.NEXT_PUBLIC_MERGE_CANVAS_CONTRACT_ADDRESS : process.env.NEXT_PUBLIC_MERGE_CANVAS_CONTRACT_ADDRESS_DEV) as string
