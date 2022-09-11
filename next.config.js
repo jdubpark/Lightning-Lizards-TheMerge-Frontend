ca712525a793284f3d90b2df5a514760b73ab0a5
yuma <mr.yuma.tanaka@gmail.com>
2022-09-11 01:42:17 -0400

    Responsive home page

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // Warning: This allows production builds to successfully complete even if
-  // your project has ESLint errors.
-  ignoreDuringBuilds: true,
-  // FROM: https://nextjs.org/docs/api-reference/next.config.js/ignoring-typescript-errors
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-}
+    reactStrictMode: true,
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+    // FROM: https://nextjs.org/docs/api-reference/next.config.js/ignoring-typescript-errors
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+    images: {
+        domains: ['merge-nft.s3.us-west-2.amazonaws.com'],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
